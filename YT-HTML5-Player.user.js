c4ac583a2cca2d2405a0b82e62a62b530d3db1ed
ktg5 <42522603+ktg5@users.noreply.github.com>
2022-02-04 09:01:38 -0800

    First commit!

---
+++ YT-HTML5-Player.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         YouTube 2012-15 HTML5 Player
+// @namespace    http://ktg5.online/
+// @version      0.1
+// @description  Try to recreate the old YouTube 2012-2015 player.
+// @author       ktg5
+// @match        *://*.youtube.com/*
+// @icon         http://dl.kt5company.com/img/yt-userscript/favicon.png
+// @resource     CSS 
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+
+// Most of the work of this script is from the CSS which is located at (CSS link on le GitHub)
+const myCss = GM_getResourceText("CSS");
+GM_addStyle(myCss);
+
+})();
