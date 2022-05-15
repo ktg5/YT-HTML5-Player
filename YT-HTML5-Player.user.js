e0ba332f9e77dee5782074f30c2f6164c33e25a5
ktg5 <42522603+ktg5@users.noreply.github.com>
2022-05-15 13:09:38 -0700

    dev -> main

--- YT-HTML5-Player.user.js
+++ YT-HTML5-Player.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         YouTube HTML5 Player
 // @namespace    https://github.com/ktg5/YT-HTML5-Player/
-// @version      1.3.1dev6
+// @version      1.3.1
 // @description  Try to recreate the old YouTube player looks.
 // @author       ktg5
 // @match        *://www.youtube.com/*
-// @updateURL    https://github.com/ktg5/YT-HTML5-Player/raw/dev/YT-HTML5-Player.user.js
-// @downloadURL  https://github.com/ktg5/YT-HTML5-Player/raw/dev/YT-HTML5-Player.user.js
+// @updateURL    https://github.com/ktg5/YT-HTML5-Player/raw/main/YT-HTML5-Player.user.js
+// @downloadURL  https://github.com/ktg5/YT-HTML5-Player/raw/main/YT-HTML5-Player.user.js
 // @icon         https://raw.githubusercontent.com/ktg5/YT-HTML5-Player/main/img/favicon.png
-// @resource     CSS https://github.com/ktg5/YT-HTML5-Player/raw/dev/style.css
-// @resource     3RD-PARTY https://github.com/ktg5/YT-HTML5-Player/raw/dev/3rd-party-style.css
-// @require      https://github.com/ktg5/YT-HTML5-Player/raw/dev/3rd-party-script.js
+// @resource     CSS https://github.com/ktg5/YT-HTML5-Player/raw/main/style.css
+// @resource     3RD-PARTY https://github.com/ktg5/YT-HTML5-Player/raw/main/3rd-party-style.css
+// @require      https://github.com/ktg5/YT-HTML5-Player/raw/main/3rd-party-script.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
